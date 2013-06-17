18127b2004f999901c67f232c7539d6615003cc1
Solomon White, Cory Flanigan, and Jim Denton <rubysolo@gmail.com>
2013-06-17 08:59:21 -0600

    add robot name test

---
+++ 06-robot-name/test.clj
@@ -0,0 +1,9 @@
+(load-file "robot.clj")
+
+(def robbie (robot))
+(assert (re-find (re-matcher #"[A-Z]{2}\d{3}" (robot-name robbie))))
+(assert (= (robot-name robbie) (robot-name robbie)))
+
+(def original-name (robot-name robbie))
+(reset-name robbie)
+(assert (not (= original-name (robot-name robbie))))
